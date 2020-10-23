0232a4459b1cca61f6cffabd2e651a0fdc86cbdd
Alanray Baltazar Elangos <48097631+Noob-Coder-Alan@users.noreply.github.com>
2020-10-23 22:25:41 +0800

    changed charater arr contents to just plain strings,a dded taglines and picture directory objects

--- script.js
+++ script.js
@@ -6,20 +6,29 @@ function openDrawer() {
     document.getElementById("menu").style.width = "0";
   }
 
-// To be modified for HTML functionality
-  const characters = [{
-    name: 'Alpha',
-    traits: ['A1', 'A2', 'A3', 'A4', 'A5']
-  }, {
-    name: 'Bravo',
-    traits: ['B1', 'B2', 'B3', 'B4', 'B5']
-  }, {
-    name: 'Charlie',
-    traits: ['C1', 'C2', 'C3', 'C4', 'C5']
-  }, {
-    name: 'Delta',
-    traits: ['D1', 'D2', 'D3', 'D4', 'D5']
-  }];
+  const characters = [
+    "Snake",
+    "Doom Slayer",
+    "Mario",
+    "Steve",
+    "Sonic"
+  ];
+  
+  const taglines = {
+    "Snake": "He who controls the battlefield, controls history.",
+    "Doom Slayer": "Rip and tear until it is done.",
+    "Mario": "It's a-me, Mario!",
+    "Steve": "Ooof!",
+    "Sonic": "Let's blast through with Sonic Speed!"
+  }
+  
+  const pictureDir = {
+    "Snake":"images/snake.jpg",
+    "Doom Slayer": "images/doomSlayer.jpg",
+    "Mario": "images/mario.jpg",
+    "Steve": "images/steve.png",
+    "Sonic": "images/sonic.jpg"
+  }
   
   let scoreboard = new Array(characters.length).fill(0);
   
